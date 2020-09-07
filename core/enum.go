14c103eb4612cd1250844db913d3adf75570d105
jirry.ji <jirenyoucn@gmail.com>
2020-09-08 00:20:41 +0800

    枚举基类

--- core/enum.go
+++ core/enum.go
@@ -6,6 +6,11 @@ type EnumType int
 // 枚举名称
 var EnumNames map[EnumType]string
 
+// 包初始化
+func init() {
+	EnumNames = make(map[EnumType]string)
+}
+
 // 枚举值
 func (e EnumType) Value() int {
 	return int(e)
